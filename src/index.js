6e43835ee67993f232fff381eb4c13db70968ad2
Konstantin Muzyka <kosatkinms@mail.ru>
2022-02-07 18:18:16 +0300

    tests corrected

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
 export default class Validator {
-	constructor(username, mobile) {
-		this.username = this.validateUsername(username);
-		this.mobileNumber = this.validateMobile(mobile);
-	}
+  constructor(username, mobile) {
+    this.username = this.validateUsername(username);
+    this.mobileNumber = this.validateMobile(mobile);
+  }
 
-	validateUsername(username) {
-		if (
-			/^[^\d-_][\w-]+[^\d-_]$/.test(username) &&
-			!/\d{4,}/.test(username)
-		) {
-			return username;
-		} else {
-			throw new Error(`
+  validateUsername(username) {
+    if (
+      /^[^\d-_][\w-]+[^\d-_]$/.test(username) && !/\d{4,}/.test(username)) {
+      return username;
+    }
+    throw new Error(`
       - Only Latin letters, dashes, underscores _ and numbers (0-9) are allowed.
       - The name must not contain more than three digits in a row, or begin or end with numbers, underscores, or dashes.`);
-		}
-	}
+  }
 
-	validateMobile(mobile) {
-		let number = mobile.replace(/[\s\-\(\)]/g, '');
+  validateMobile(mobile) {
+    const number = mobile.replace(/[\s\-()]/g, '');
 
-		if (number.length > 13 || number.length < 11) {
-			throw new Error(`An invalid number ${mobile} was entered`);
-		}
+    if (number.length > 13 || number.length < 11) {
+      throw new Error(`An invalid number ${mobile} was entered`);
+    }
 
-		if (number.length === 12 && !/\+/.test(number)) {
-			return number.replace(/(^\d)/, '+$1');
-		} else if (
-			(number.length === 12 && /\+/.test(number)) ||
-			number.length === 11
-		) {
-			return number.replace(/^([^+]|8)/g, '+7');
-		} else {
-			return number;
-		}
-	}
+    if (number.length === 12 && !/\+/.test(number)) {
+      return number.replace(/(^\d)/, '+$1');
+    }
+    if (
+      (number.length === 12 && /\+/.test(number)) || number.length === 11) {
+      return number.replace(/^([^+]|8)/g, '+7');
+    }
+    return number;
+  }
 }
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,46 +1,38 @@
 import Validator from '../index';
 
 test('Validator should return correctly validate username and mobile number', () => {
-	let user1 = new Validator('nero_99rus', '8 (927) 000-00-00');
-	let user2 = new Validator('dante999-999jj', '+7 960 000 00 00');
-	let user3 = new Validator('n9n9_9n', '86 000 000 0000');
-  let user4 = new Validator('rus178rus', '+86 000 000 0000');
+  const user1 = new Validator('nero_99rus', '8 (927) 000-00-00');
+  const user2 = new Validator('dante999-999jj', '+7 960 000 00 00');
+  const user3 = new Validator('n9n9_9n', '86 000 000 0000');
+  const user4 = new Validator('rus178rus', '+86 000 000 0000');
 
-	expect(user1).toEqual({
-		username: 'nero_99rus',
-		mobileNumber: '+79270000000',
-	});
+  expect(user1).toEqual({
+    username: 'nero_99rus',
+    mobileNumber: '+79270000000',
+  });
 
-	expect(user2).toEqual({
-		username: 'dante999-999jj',
-		mobileNumber: '+79600000000',
-	});
+  expect(user2).toEqual({
+    username: 'dante999-999jj',
+    mobileNumber: '+79600000000',
+  });
 
-	expect(user3).toEqual({
-		username: 'n9n9_9n',
-		mobileNumber: '+860000000000',
-	});
+  expect(user3).toEqual({
+    username: 'n9n9_9n',
+    mobileNumber: '+860000000000',
+  });
 
   expect(user4).toEqual({
-		username: 'rus178rus',
-		mobileNumber: '+860000000000',
-	});
+    username: 'rus178rus',
+    mobileNumber: '+860000000000',
+  });
 });
 
 test('validateUsername should throw an error, if an invalid username is passed', () => {
-	let invalidUser1 = new Validator('nero1111nero', '8 (927) 000-00-00');
-	let invalidUser2 = new Validator('_-nero999', '8 (927) 000-00-00');
-	let invalidUser3 = new Validator('nero99 nero', '8 (927) 000-00-00');
-
-	expect(() => invalidUser1).toThrow(Error);
-
-  expect(() => invalidUser2).toThrow(Error);
-
-  expect(() => invalidUser3).toThrow(Error);
+  expect(() => new Validator('nero1111nero', '+79999999999')).toThrow(Error);
+  expect(() => new Validator('_-nero999', '+79999999999')).toThrow(Error);
+  expect(() => new Validator('nero99 nero', '+79999999999')).toThrow(Error);
 });
 
 test('validateMobile should throw an error, if an invalid mobile is passed', () => {
-	let invalidUser = new Validator('bob', '88 990 999 9999 2211 1');
-
-	expect(() => invalidUser).toThrow(Error);
+  expect(() => new Validator('bob', '88 999 999 999 999 88')).toThrow(Error);
 });
